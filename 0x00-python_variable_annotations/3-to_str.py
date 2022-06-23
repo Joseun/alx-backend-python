7e5f69a9445a4c135d2596e03e6df2fa9eb4a3db
Joseun <ologanj@gmail.com>
2022-06-23 16:12:05 +0000

    Python - Variable Annotations

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -7,7 +7,7 @@
 
 def to_str(n: float) -> str:
     """
-    takes a float n as argument and returns 
+    takes a float n as argument and returns
     the string representation of the float
 
     Arguments:
