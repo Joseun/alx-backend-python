2b8c062578b0aa1df03c7997823bdb6c58ae8016
Joseun <ologanj@gmail.com>
2022-06-28 02:09:38 +0000

    Python - Async

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" an asynchronous coroutine  """
+import asyncio
+import random
+import time
+from typing import List
+task_wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    an asynchronous coroutine
+    
+    Argument:
+    n: integer
+    max_delay: integer
+    """
+    x = [asyncio.create_task(task_wait_random(max_delay)) for i in range(n)]
+    finish = [await task for task in asyncio.as_completed(x)]
+    return finish
